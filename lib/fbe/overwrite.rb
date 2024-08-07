57246ebbbbe4c6e44945e6259ca93f33d265cb19
Yegor Bugayenko <yegor256@gmail.com>
2024-08-07 21:54:21 +0300

    check

--- lib/fbe/overwrite.rb
+++ lib/fbe/overwrite.rb
@@ -38,6 +38,7 @@ require_relative 'fb'
 # @param [String] property The name of the property to set
 # @param [Any] vqlue The value to set
 def Fbe.overwrite(fact, property, value, fb: Fbe.fb)
+  raise 'The fact is nil' if fact.nil?
   before = {}
   fact.all_properties.each do |prop|
     before[prop.to_s] = fact[prop]
