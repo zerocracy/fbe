dd16990e4b2c104f631cd7ab8b15a736151ec9b3
Yegor Bugayenko <yegor256@gmail.com>
2024-10-13 11:29:37 +0300

    explained

--- lib/fbe/unmask_repos.rb
+++ lib/fbe/unmask_repos.rb
@@ -22,40 +22,54 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+require_relative '../fbe'
 require_relative 'octo'
 
-# Unmask.
-# Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2024 Zerocracy
-# License:: MIT
-module Fbe
-  def self.mask_to_regex(mask)
-    org, repo = mask.split('/')
-    raise "Org '#{org}' can't have an asterisk" if org.include?('*')
-    Regexp.compile("#{org}/#{repo.gsub('*', '.*')}")
-  end
+# Converts mask to repository name.
+#
+# This function takes something like +"zerocracy/*"+ as an input and returns
+# a regular expression that may match repositories defined by this mask, which
+# is +/zerocracy\/.*+ in this particular case.
+#
+# @param [String] mask The mask
+# @return [Regex] Regular expression
+def Fbe.mask_to_regex(mask)
+  org, repo = mask.split('/')
+  raise "Org '#{org}' can't have an asterisk" if org.include?('*')
+  Regexp.compile("#{org}/#{repo.gsub('*', '.*')}")
+end
 
-  def self.unmask_repos(options: $options, global: $global, loog: $loog)
-    repos = []
-    octo = Fbe.octo(loog:, global:, options:)
-    masks = (options.repositories || '').split(',')
-    masks.reject { |m| m.start_with?('-') }.each do |mask|
-      unless mask.include?('*')
-        repos << mask
-        next
-      end
-      re = Fbe.mask_to_regex(mask)
-      octo.repositories(mask.split('/')[0]).each do |r|
-        repos << r[:full_name] if re.match?(r[:full_name])
-      end
+# Builds a list of repositories required by the +repositories+ option.
+#
+# The +repositories+ option defined in the +$options+ must contain something
+# like "zerocracy/fbe,zerocracy/ab*" (comma-separated list of masks). This
+# function will go to the GitHub API and fetch all available repositories
+# by these masks.
+#
+# @param [Judges::Options] options The options coming from the +judges+ tool
+# @param [Hash] global The hash for global caching
+# @param [Loog] loog The logging facility
+# @return [Array<String>] List of repository full names
+def Fbe.unmask_repos(options: $options, global: $global, loog: $loog)
+  repos = []
+  octo = Fbe.octo(loog:, global:, options:)
+  masks = (options.repositories || '').split(',')
+  masks.reject { |m| m.start_with?('-') }.each do |mask|
+    unless mask.include?('*')
+      repos << mask
+      next
     end
-    masks.select { |m| m.start_with?('-') }.each do |mask|
-      re = Fbe.mask_to_regex(mask[1..])
-      repos.reject! { |r| re.match?(r) }
+    re = Fbe.mask_to_regex(mask)
+    octo.repositories(mask.split('/')[0]).each do |r|
+      repos << r[:full_name] if re.match?(r[:full_name])
     end
-    repos.reject! { |repo| octo.repository(repo)[:archived] }
-    raise "No repos found matching: #{options.repositories}" if repos.empty?
-    loog.debug("Scanning #{repos.size} repositories: #{repos.join(', ')}...")
-    repos
   end
+  masks.select { |m| m.start_with?('-') }.each do |mask|
+    re = Fbe.mask_to_regex(mask[1..])
+    repos.reject! { |r| re.match?(r) }
+  end
+  repos.reject! { |repo| octo.repository(repo)[:archived] }
+  raise "No repos found matching: #{options.repositories}" if repos.empty?
+  loog.debug("Scanning #{repos.size} repositories: #{repos.join(', ')}...")
+  repos
 end
