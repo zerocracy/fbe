223ed2f24fc63b57e5ce3e5e83285d0d8371aeba
Yegor Bugayenko <yegor256@gmail.com>
2024-10-14 13:32:44 +0300

    #113 Fbe.enter

---
+++ lib/fbe/enter.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# MIT License
+#
+# Copyright (c) 2024 Zerocracy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'baza-rb'
+require_relative '../fbe'
+
+# Enter a new valve.
+#
+# @param [String] badge Unique badge of the valve
+# @param [String] why The reason
+# @param [Judges::Options] options The options coming from the +judges+ tool
+# @param [Loog] loog The logging facility
+# @return [String] Full name of the user
+def Fbe.enter(badge, why, options: $options, loog: $loog, &)
+  baza = BazaRb.new('api.zerocracy.com', 443, options.zerocracy_token, loog:)
+  baza.enter(options.job_name, badge, why, options.job_id.to_i, &)
+end
