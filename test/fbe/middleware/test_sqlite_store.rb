c8a152311a5ffa494ff08eb5b4015c80e7fbecbe
Artem Yegorov <yegorov0725@yandex.ru>
2025-06-06 19:49:18 +0300

    #197: fix sqlite tests

--- test/fbe/middleware/test_sqlite_store.rb
+++ test/fbe/middleware/test_sqlite_store.rb
@@ -12,33 +12,24 @@ require_relative '../../../lib/fbe/middleware/sqlite_store'
 # Copyright:: Copyright (c) 2024-2025 Zerocracy
 # License:: MIT
 class SqliteStoreTest < Fbe::Test
-  attr_reader :store
-
-  def self.store
-    @store ||=
-      begin
-        path = File.expand_path('../../../.test.db1', __dir__)
-        Fbe::Middleware::SqliteStore.new(path)
-      end
-  end
-
-  def setup
-    @store = self.class.store
-    store.clear
-  end
-
   def test_sqlite_store
-    assert_nil(store.read('my_key'))
-    assert_nil(store.delete('my_key'))
-    assert_nil(store.write('my_key', 'some value'))
-    assert_equal('some value', store.read('my_key'))
-    assert_nil(store.write('my_key', 'some value 2'))
-    assert_equal('some value 2', store.read('my_key'))
-    assert_nil(store.delete('my_key'))
-    assert_nil(store.read('my_key'))
+    Dir.mktmpdir do |dir|
+      store = Fbe::Middleware::SqliteStore.new(File.expand_path('test.db', dir))
+      assert_nil(store.read('my_key'))
+      assert_nil(store.delete('my_key'))
+      assert_nil(store.write('my_key', 'some value'))
+      assert_equal('some value', store.read('my_key'))
+      assert_nil(store.write('my_key', 'some value 2'))
+      assert_equal('some value 2', store.read('my_key'))
+      assert_nil(store.delete('my_key'))
+      assert_nil(store.read('my_key'))
+    end
   end
 
   def test_sqlite_store_empty_all
-    assert_empty(store.all)
+    Dir.mktmpdir do |dir|
+      store = Fbe::Middleware::SqliteStore.new(File.expand_path('test.db', dir))
+      assert_empty(store.all)
+    end
   end
 end
