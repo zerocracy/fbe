d5f992ac44dd15472bc9c48f62cafc2d1236a9bd
Artem Yegorov <yegorov0725@yandex.ru>
2025-06-08 13:28:29 +0300

    #199: refactor ctor in `Fbe::Middleware::SqliteStore`

--- test/fbe/middleware/test_sqlite_store.rb
+++ test/fbe/middleware/test_sqlite_store.rb
@@ -13,8 +13,7 @@ require_relative '../../../lib/fbe/middleware/sqlite_store'
 # License:: MIT
 class SqliteStoreTest < Fbe::Test
   def test_simple_caching_algorithm
-    Dir.mktmpdir do |dir|
-      f = File.expand_path('x.db', dir)
+    with_tmpfile('x.db') do |f|
       store = Fbe::Middleware::SqliteStore.new(f)
       k = 'some-key'
       assert_nil(store.read(k))
@@ -32,20 +31,79 @@ class SqliteStoreTest < Fbe::Test
   end
 
   def test_returns_empty_list
-    Dir.mktmpdir do |dir|
-      store = Fbe::Middleware::SqliteStore.new(File.expand_path('b.db', dir))
+    with_tmpfile('b.db') do |f|
+      store = Fbe::Middleware::SqliteStore.new(f)
       assert_empty(store.all)
     end
   end
 
   def test_drops_all_keys
-    Dir.mktmpdir do |dir|
-      store = Fbe::Middleware::SqliteStore.new(File.expand_path('a.db', dir))
+    with_tmpfile('a.db') do |f|
+      store = Fbe::Middleware::SqliteStore.new(f)
       k = 'a key'
       store.write(k, 'some value')
       store.drop
       store.prepare
       assert_empty(store.all)
+    end
+  end
+
+  def test_read_from_wrong_db_path
+    assert_raises(ArgumentError) do
+      Fbe::Middleware::SqliteStore.new(nil).read('my_key')
+    end
+    assert_raises(ArgumentError) do
+      Fbe::Middleware::SqliteStore.new('').read('my_key')
+    end
+    assert_raises(ArgumentError) do
+      Fbe::Middleware::SqliteStore.new('/fakepath/fakefolder/test.db').read('my_key')
+    end
+  end
+
+  def test_empty_all_if_not_written
+    with_tmpfile do |f|
+      store = Fbe::Middleware::SqliteStore.new(f)
+      assert_empty(store.all)
+    end
+  end
+
+  def test_setup
+    with_tmpfile do |path|
+      store = Fbe::Middleware::SqliteStore.new(path)
+      refute_predicate store, :setup?
+      2.times { store.setup }
+      assert_predicate store, :setup?
+      store.drop
+      refute_predicate store, :setup?
+      2.times { store.setup }
+      assert_predicate store, :setup?
+    end
+  end
+
+  def test_close
+    with_tmpfile do |path|
+      store = Fbe::Middleware::SqliteStore.new(path)
+      store.write('my_key', 'my_value')
+      2.times { store.close }
+      assert_equal('my_value', store.read('my_key'))
+    end
+  end
+
+  def test_clear
+    with_tmpfile do |path|
+      store = Fbe::Middleware::SqliteStore.new(path)
+      store.write('my_key', 'my_value')
+      assert_equal('my_value', store.read('my_key'))
+      store.clear
+      assert_nil(store.read('my_key'))
+    end
+  end
+
+  private
+
+  def with_tmpfile(name = 'test.db', &)
+    Dir.mktmpdir do |dir|
+      yield File.expand_path(name, dir)
     end
   end
 end
