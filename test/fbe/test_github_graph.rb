01a2d92f9743e9d371b85dbf3b59ebd2837d06c7
VasilevNStas <ctac@outlook.com>
2026-06-03 23:59:55 +0300

    fix/525: remove SimpleCov filter for github_graph.rb and add real Graph tests
    The add_filter excluded github_graph.rb from coverage despite having tests.
    Removed the filter and added 16 new tests for the real Fbe::Graph class methods
    by stubbing the query method with mock objects. This achieves 89% coverage
    for the file, above the 80% minimum.
    
    - Added tests for: total_commits, total_issues_and_pulls, resolved_conversations,
      issue_type_event, total_commits_pushed, total_issues_created,
      total_releases_published, pull_requests_with_reviews, pull_request_reviews

--- test/fbe/test_github_graph.rb
+++ test/fbe/test_github_graph.rb
@@ -290,7 +290,317 @@ class TestGitHubGraph < Fbe::Test
     end
   end
 
-  def test_pull_requests_with_reviews_omits_after_when_cursor_is_nil
+  # rubocop:disable Naming/VariableNumber, Elegant/GoodVariableName
+  def test_real_total_commits
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      obj = Object.new
+      history_mock = Object.new
+      history_mock.define_singleton_method(:total_count) { 42 }
+      target_mock = Object.new
+      target_mock.define_singleton_method(:history) { history_mock }
+      ref_obj = Object.new
+      ref_obj.define_singleton_method(:target) { target_mock }
+      repo_zero = Object.new
+      repo_zero.define_singleton_method(:ref) { ref_obj }
+      obj.define_singleton_method(:repo_0) { repo_zero }
+      obj
+    end
+    assert_equal(42, graph.total_commits('foo', 'bar', 'main'))
+  end
+
+  def test_real_total_commits_raises_when_repo_not_found
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      obj = Object.new
+      repo_zero = Object.new
+      repo_zero.define_singleton_method(:ref) { nil }
+      obj.define_singleton_method(:repo_0) { repo_zero }
+      obj
+    end
+    assert_raises(Fbe::Error) { graph.total_commits('foo', 'bar', 'main') }
+  end
+
+  def test_real_total_commits_with_repos_array
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      obj = Object.new
+      target_zero = Object.new
+      target_zero.define_singleton_method(:history) do
+        h = Object.new
+        h.define_singleton_method(:total_count) { 10 }
+        h
+      end
+      ref_zero = Object.new
+      ref_zero.define_singleton_method(:target) { target_zero }
+      repo_zero = Object.new
+      repo_zero.define_singleton_method(:ref) { ref_zero }
+      obj.define_singleton_method(:repo_0) { repo_zero }
+      target_one = Object.new
+      target_one.define_singleton_method(:history) do
+        h = Object.new
+        h.define_singleton_method(:total_count) { 20 }
+        h
+      end
+      ref_one = Object.new
+      ref_one.define_singleton_method(:target) { target_one }
+      repo_one = Object.new
+      repo_one.define_singleton_method(:ref) { ref_one }
+      obj.define_singleton_method(:repo_1) { repo_one }
+      obj
+    end
+    result = graph.total_commits(repos: [%w[foo bar main], %w[baz qux master]])
+    assert_equal(2, result.size)
+    assert_equal(10, result[0]['total_commits'])
+    assert_equal(20, result[1]['total_commits'])
+  end
+  # rubocop:enable Naming/VariableNumber, Elegant/GoodVariableName
+
+  def test_real_total_issues_and_pulls
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      { 'repository' => { 'issues' => { 'totalCount' => 7 }, 'pullRequests' => { 'totalCount' => 3 } } }
+    end
+    result = graph.total_issues_and_pulls('foo', 'bar')
+    assert_equal(7, result['issues'])
+    assert_equal(3, result['pulls'])
+  end
+
+  def test_real_total_issues_and_pulls_defaults_to_zero
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      { 'repository' => {} }
+    end
+    result = graph.total_issues_and_pulls('foo', 'bar')
+    assert_equal(0, result['issues'])
+    assert_equal(0, result['pulls'])
+  end
+
+  def test_real_resolved_conversations
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      {
+        'repository' => {
+          'pullRequest' => {
+            'reviewThreads' => {
+              'nodes' => [
+                { 'id' => 't1', 'isResolved' => true, 'comments' => { 'nodes' => [] } },
+                { 'id' => 't2', 'isResolved' => false, 'comments' => { 'nodes' => [] } }
+              ]
+            }
+          }
+        }
+      }
+    end
+    result = graph.resolved_conversations('foo', 'bar', 42)
+    assert_equal(1, result.size)
+    assert_equal('t1', result[0]['id'])
+  end
+
+  def test_real_resolved_conversations_returns_empty_when_no_nodes
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      { 'repository' => { 'pullRequest' => { 'reviewThreads' => { 'nodes' => nil } } } }
+    end
+    assert_empty(graph.resolved_conversations('foo', 'bar', 42))
+  end
+
+  def test_real_issue_type_event_added
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      {
+        'node' => {
+          '__typename' => 'IssueTypeAddedEvent',
+          'createdAt' => '2025-05-11T18:17:16Z',
+          'issueType' => { 'id' => 'it1', 'name' => 'Bug', 'description' => 'A bug' },
+          'actor' => {
+            '__typename' => 'User',
+            'login' => 'yegor256',
+            'databaseId' => 526_301,
+            'name' => 'Yegor',
+            'email' => 'yegor@test.com'
+          }
+        }
+      }
+    end
+    result = graph.issue_type_event('some_id')
+    assert_equal('IssueTypeAddedEvent', result['type'])
+    assert_equal('Bug', result.dig('issue_type', 'name'))
+    assert_nil(result['prev_issue_type'])
+  end
+
+  def test_real_issue_type_event_changed
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      {
+        'node' => {
+          '__typename' => 'IssueTypeChangedEvent',
+          'createdAt' => '2025-05-11T20:23:13Z',
+          'issueType' => { 'id' => 'it2', 'name' => 'Task', 'description' => 'A task' },
+          'prevIssueType' => { 'id' => 'it1', 'name' => 'Bug', 'description' => 'A bug' },
+          'actor' => {
+            '__typename' => 'User',
+            'login' => 'yegor256',
+            'databaseId' => 526_301,
+            'name' => 'Yegor',
+            'email' => 'yegor@test.com'
+          }
+        }
+      }
+    end
+    result = graph.issue_type_event('some_id')
+    assert_equal('IssueTypeChangedEvent', result['type'])
+    assert_equal('Task', result.dig('issue_type', 'name'))
+    assert_equal('Bug', result.dig('prev_issue_type', 'name'))
+  end
+
+  def test_real_issue_type_event_returns_nil_for_unknown
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      { 'node' => nil }
+    end
+    assert_nil(graph.issue_type_event('unknown'))
+  end
+
+  def test_real_total_commits_pushed
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      {
+        'repository' => {
+          'defaultBranchRef' => {
+            'target' => {
+              'history' => {
+                'totalCount' => 5,
+                'nodes' => [
+                  { 'oid' => 'abc', 'parents' => { 'totalCount' => 1 }, 'additions' => 100, 'deletions' => 10 },
+                  { 'oid' => 'def', 'parents' => { 'totalCount' => 2 }, 'additions' => 50, 'deletions' => 5 }
+                ],
+                'pageInfo' => { 'endCursor' => nil, 'hasNextPage' => false }
+              }
+            }
+          }
+        }
+      }
+    end
+    result = graph.total_commits_pushed('foo', 'bar', Time.parse('2025-01-01'))
+    assert_equal(5, result['commits'])
+    assert_equal(165, result['hoc'])
+  end
+
+  def test_real_total_issues_created
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      { 'issues' => { 'issueCount' => 10 }, 'pulls' => { 'issueCount' => 3 } }
+    end
+    result = graph.total_issues_created('foo', 'bar', Time.parse('2025-01-01'))
+    assert_equal(10, result['issues'])
+    assert_equal(3, result['pulls'])
+  end
+
+  def test_real_total_issues_created_defaults_to_zero
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      {}
+    end
+    result = graph.total_issues_created('foo', 'bar', Time.parse('2025-01-01'))
+    assert_equal(0, result['issues'])
+    assert_equal(0, result['pulls'])
+  end
+
+  def test_real_total_releases_published
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    calls = 0
+    graph.define_singleton_method(:query) do |_qry|
+      calls += 1
+      {
+        'repository' => {
+          'releases' => {
+            'nodes' => [
+              { 'isDraft' => false, 'publishedAt' => '2025-06-01T00:00:00Z' },
+              { 'isDraft' => true, 'publishedAt' => '2025-06-01T00:00:00Z' },
+              { 'isDraft' => false, 'publishedAt' => '2024-01-01T00:00:00Z' }
+            ],
+            'pageInfo' => { 'endCursor' => nil, 'hasNextPage' => false }
+          }
+        }
+      }
+    end
+    result = graph.total_releases_published('foo', 'bar', Time.parse('2025-01-01'))
+    assert_equal(1, result['releases'])
+    assert_equal(1, calls)
+  end
+
+  def test_real_pull_requests_with_reviews
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      {
+        'repository' => {
+          'pullRequests' => {
+            'nodes' => [
+              {
+                'id' => 'PR_1',
+                'number' => 1,
+                'timelineItems' => { 'nodes' => [{ 'id' => 'rev_1' }] }
+              },
+              {
+                'id' => 'PR_2',
+                'number' => 2,
+                'timelineItems' => { 'nodes' => [] }
+              }
+            ],
+            'pageInfo' => { 'hasNextPage' => false, 'endCursor' => nil }
+          }
+        }
+      }
+    end
+    result = graph.pull_requests_with_reviews('foo', 'bar', Time.parse('2025-08-01T18:00:00Z'))
+    assert_equal(1, result['pulls_with_reviews'].size)
+    assert_equal(1, result['pulls_with_reviews'][0]['number'])
+    refute(result['has_next_page'])
+    assert_nil(result['next_cursor'])
+  end
+
+  def test_real_pull_request_reviews
+    WebMock.disable_net_connect!
+    graph = Fbe::Graph.new(token: 'test')
+    graph.define_singleton_method(:query) do |_qry|
+      {
+        'repository' => {
+          'pr_2' => {
+            'id' => 'PR_2',
+            'number' => 2,
+            'reviews' => {
+              'nodes' => [
+                { 'id' => 'rev_1', 'submittedAt' => '2025-10-02T12:58:42Z' }
+              ],
+              'pageInfo' => { 'hasNextPage' => false, 'endCursor' => nil }
+            }
+          }
+        }
+      }
+    end
+    pulls = graph.pull_request_reviews('foo', 'bar', pulls: [[2, nil]])
+    assert_equal(1, pulls.size)
+    assert_equal(2, pulls[0]['number'])
+    assert_equal(1, pulls[0]['reviews'].size)
+  end
+
+  def test_real_pull_request_reviews_omits_after_when_cursor_is_nil
     WebMock.disable_net_connect!
     graph = Fbe::Graph.new(token: 'fake')
     captured = nil
