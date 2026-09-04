dd826ced376462e48e98fd6ab4b07548be3c13c7
morphqdd <europeDreadlyDevil@gmail.com>
2026-09-04 09:11:33 +0300

    #682: render an unknown term instead of raising in to_s (#703)

--- lib/fbe/award.rb
+++ lib/fbe/award.rb
@@ -258,7 +258,7 @@ class Fbe::Award
         "#{to_p(@operands[0])} clamped between #{to_p(@operands[1])} and #{to_p(@operands[2])}, " \
         "or 0 if it is smaller than #{to_p(@operands[1])}"
       else
-        raise(Fbe::Error, "Unknown term '#{@op}'")
+        "(#{@op} #{@operands.join(' ')})"
       end
     end
 
