51ca934d45d39d64a059803f9f41d082a9497ed2
morphqdd <morphqdd@gmail.com>
2026-09-26 13:22:28 +0000

    #797: keep the complexity cops quiet on if_absent

--- lib/fbe/if_absent.rb
+++ lib/fbe/if_absent.rb
@@ -49,7 +49,7 @@ require_relative 'same'
 #   else
 #     puts "User already exists"
 #   end
-def Fbe.if_absent(fb: Fbe.fb, always: false) # rubocop:disable Metrics/PerceivedComplexity
+def Fbe.if_absent(fb: Fbe.fb, always: false) # rubocop:disable Metrics/PerceivedComplexity,Metrics/AbcSize,Metrics/CyclomaticComplexity
   raise(Fbe::Error, 'A block is required by if_absent') unless block_given?
   attrs = {}
   f =
