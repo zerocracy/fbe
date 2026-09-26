5847fd05152bff0c5017ab1a513d84f1272dfdb1
morphqdd <europeDreadlyDevil@gmail.com>
2026-09-26 09:33:25 +0300

    #1095: accept Float identifiers in Fbe.who and Fbe.issue (#1125)
    * #1095: accept Float identifiers in Fbe.who and Fbe.issue
    
    * #1095: test Float and non-numeric identifiers in who and issue

--- test/fbe/test_issue.rb
+++ test/fbe/test_issue.rb
@@ -23,4 +23,24 @@ class TestIssue < Fbe::Test
     options = Judges::Options.new({ 'testing' => true })
     assert_equal('yegor256/test#333', Fbe.issue(f, global:, options:, loog: Loog::NULL))
   end
+
+  def test_with_float_ids
+    fb = Factbase.new
+    f = fb.insert
+    f.repository = 323.0
+    f.issue = 333.0
+    global = {}
+    options = Judges::Options.new({ 'testing' => true })
+    assert_equal('yegor256/test#333', Fbe.issue(f, global:, options:, loog: Loog::NULL))
+  end
+
+  def test_with_non_numeric_repository
+    fb = Factbase.new
+    f = fb.insert
+    f.repository = 'yegor'
+    f.issue = 333
+    global = {}
+    options = Judges::Options.new({ 'testing' => true })
+    assert_raises(Fbe::Error) { Fbe.issue(f, global:, options:, loog: Loog::NULL) }
+  end
 end
--- test/fbe/test_who.rb
+++ test/fbe/test_who.rb
@@ -22,4 +22,22 @@ class TestWho < Fbe::Test
     options = Judges::Options.new({ 'testing' => true })
     assert_equal('@torvalds', Fbe.who(f, global:, options:, loog: Loog::NULL))
   end
+
+  def test_with_float_id
+    fb = Factbase.new
+    f = fb.insert
+    f.who = 444.0
+    global = {}
+    options = Judges::Options.new({ 'testing' => true })
+    assert_equal('@torvalds', Fbe.who(f, global:, options:, loog: Loog::NULL))
+  end
+
+  def test_with_non_numeric_id
+    fb = Factbase.new
+    f = fb.insert
+    f.who = 'yegor'
+    global = {}
+    options = Judges::Options.new({ 'testing' => true })
+    assert_raises(Fbe::Error) { Fbe.who(f, global:, options:, loog: Loog::NULL) }
+  end
 end
